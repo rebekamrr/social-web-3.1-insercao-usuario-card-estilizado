d7e02908842dad324bba9fb56a66806f16396822
rebek <rebekamrr01@outlook.com>
2021-08-21 03:05:17 -0300

    Initial commit

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [MenuComponent],
+  exports: [
+    MenuComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    CommonModule,
+    MatMenuModule,
+    MatButtonModule
+  ]
+})
+export class LayoutModule { }
